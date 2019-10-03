88e4a126cbf36d733fc0586ba643cb848587090f
t.takahashi <takahashi.developer@gmail.com>
2019-10-03 23:45:21 +0900

    add node_modules layer

--- lambda-deploy.sh
+++ lambda-deploy.sh
@@ -4,17 +4,25 @@ set -euo pipefail
 
 readonly PREFIX="sample-secrets-manager"
 
-readonly TEMPLATE="`pwd`/setup/template.yaml"
+readonly LAYER_PATH="./lambda/layer_modules"
+readonly LAMBDA_SRC_PATH="./lambda/src"
+readonly SRC_TEMPLATE="./lambda/template.yaml"
+readonly DST_TEMPLATE="./lambda/packaged.yaml"
 readonly BUCKETNAME="${PREFIX}-artifict"
 readonly STACKNAME="${PREFIX}-lambda"
 
+mkdir -p ${LAYER_PATH}
+cp ${LAMBDA_SRC_PATH}/package.json ${LAYER_PATH}
+cp ${LAMBDA_SRC_PATH}/package-lock.json ${LAYER_PATH}
+npm install --production --prefix ${LAYER_PATH}
+
 sam package \
-  --template-file ./lambda/template.yaml \
-  --output-template-file ./lambda/packaged.yaml \
+  --template-file ${SRC_TEMPLATE} \
+  --output-template-file ${DST_TEMPLATE} \
   --s3-bucket ${BUCKETNAME}
 
 sam deploy \
-  --template-file ./lambda/packaged.yaml \
+  --template-file ${DST_TEMPLATE} \
   --stack-name ${STACKNAME} \
   --capabilities CAPABILITY_IAM \
   --parameter-overrides \
