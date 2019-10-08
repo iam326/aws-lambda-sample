2a58d818843c103958a0e732387766094aa341f3
t.takahashi <takahashi.developer@gmail.com>
2019-10-08 23:26:38 +0900

    fix layer

--- lambda-deploy.sh
+++ lambda-deploy.sh
@@ -4,16 +4,16 @@ set -euo pipefail
 
 readonly PREFIX="sample-secrets-manager"
 
-readonly LAYER_PATH="./lambda/layer_modules"
-readonly LAMBDA_SRC_PATH="./lambda/src"
-readonly SRC_TEMPLATE="./lambda/template.yaml"
-readonly DST_TEMPLATE="./lambda/packaged.yaml"
+readonly LAMBDA_DIR="./lambda"
+readonly LAYER_PATH="${LAMBDA_DIR}/dependencies/nodejs"
+readonly SRC_TEMPLATE="${LAMBDA_DIR}/template.yaml"
+readonly DST_TEMPLATE="${LAMBDA_DIR}/packaged.yaml"
 readonly BUCKETNAME="${PREFIX}-artifict"
 readonly STACKNAME="${PREFIX}-lambda"
 
 mkdir -p ${LAYER_PATH}
-cp ${LAMBDA_SRC_PATH}/package.json ${LAYER_PATH}
-cp ${LAMBDA_SRC_PATH}/package-lock.json ${LAYER_PATH}
+cp ${LAMBDA_DIR}/package.json ${LAYER_PATH}
+cp ${LAMBDA_DIR}/package-lock.json ${LAYER_PATH}
 npm install --production --prefix ${LAYER_PATH}
 
 sam package \
